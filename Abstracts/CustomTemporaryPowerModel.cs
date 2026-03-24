dbbbf8af20ecdbca4e33d5f63b9702f1e24abd5c
quipsol <245758987+quipsol@users.noreply.github.com>
2026-03-24 13:23:19 +0100

    RemoveLocalization from base class
    Moves the localization out of the base class and into the Wrapper to better separate core functionality and quality of life

--- Abstracts/CustomTemporaryPowerModel.cs
+++ Abstracts/CustomTemporaryPowerModel.cs
@@ -20,7 +20,6 @@ public abstract class CustomTemporaryPowerModel : CustomPowerModel, ITemporaryPo
     protected abstract Func<Creature, decimal, Creature?, CardModel?, bool, Task> ApplyPowerFunc { get; }
     public abstract PowerModel InternallyAppliedPower { get; }
     public abstract AbstractModel OriginModel { get; }
-    protected virtual bool UseOwnLocalization => false;
     protected virtual bool UntilEndOfOtherSideTurn => false;
     protected virtual int LastForXExtraTurns => 0;
     
@@ -35,7 +34,8 @@ public abstract class CustomTemporaryPowerModel : CustomPowerModel, ITemporaryPo
     private bool _shouldIgnoreNextInstance;
     public void IgnoreNextInstance() => _shouldIgnoreNextInstance = true;
     
-
+    // Only used for localization purposes
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new RepeatVar(0), new BoolVar(LocTurnEndBoolVar, false)];
 
     public override async Task BeforeApplied(Creature target, decimal amount, Creature? applier, CardModel? cardSource)
     {
@@ -78,89 +78,5 @@ public abstract class CustomTemporaryPowerModel : CustomPowerModel, ITemporaryPo
         await ApplyPowerFunc(powerSource.Owner, -powerSource.Amount, powerSource.Owner, null, true);
         await PowerCmd.Remove(powerSource);
     }
-
-
-    #region Localization
-
-    // Only used for localization purposes
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new RepeatVar(0), new BoolVar(LocTurnEndBoolVar, false)];
-
-    public override LocString Title
-    {
-        get
-        {
-            switch (OriginModel)
-            {
-                case CardModel cardModel:
-                    return cardModel.TitleLocString;
-                case PotionModel potionModel:
-                    return potionModel.Title;
-                case RelicModel relicModel:
-                    return relicModel.Title;
-                case PowerModel powerModel:
-                    return powerModel.Title;
-                case OrbModel orbModel:
-                    return orbModel.Title;
-                case CharacterModel characterModel:
-                    return characterModel.Title;
-                case MonsterModel monsterModel:
-                    return monsterModel.Title;
-                default:
-                    MainFile.Logger.Warn($"Getting the 'Title' for the base model type of '{OriginModel.GetType().Name}' has not been implemented yet. Using default title.");
-                    return new LocString("powers",  "BASELIB-CUSTOM_TEMPORARY_POWER_MODEL.title");
-            }
-        }
-    }
-
-    protected override IEnumerable<IHoverTip> ExtraHoverTips
-    {
-        get
-        {
-            List<IHoverTip> items;
-            switch (OriginModel)
-            {
-                case CardModel card:
-                    items = [HoverTipFactory.FromCard(card)];
-                    break;
-                case PotionModel model:
-                    items = [HoverTipFactory.FromPotion(model)];
-                    break;
-                case RelicModel relic:
-                    items = HoverTipFactory.FromRelic(relic).ToList();
-                    break;
-                case PowerModel power:
-                    items = [HoverTipFactory.FromPower(power)];
-                    break;
-                default:
-                    MainFile.Logger.Warn($"Getting the Hover Tips for the base model type of '{OriginModel.GetType().Name}' has not been implemented yet.");
-                    items = [];
-                    break;
-            }
-            items.Add(HoverTipFactory.FromPower(InternallyAppliedPower));
-            return items;
-        }
-    }
-
-    public override LocString Description
-    {
-        get
-        {
-            if (UseOwnLocalization)
-                return new LocString("powers", Amount > 0 ? $"{Id.Entry}.UP.description" : $"{Id.Entry}.DOWN.description");
-            return new LocString("powers", Amount > 0 ? "BASELIB-CUSTOM_TEMPORARY_POWER_MODEL.UP.description" : "BASELIB-CUSTOM_TEMPORARY_POWER_MODEL.DOWN.description");
-        }
-    }
-    
-    protected override string SmartDescriptionLocKey
-    {
-        get
-        {
-            if (UseOwnLocalization)
-                return Amount > 0 ? $"{Id.Entry}.UP.smartDescription" : $"{Id.Entry}.DOWN.smartDescription";
-            return Amount > 0 ? "BASELIB-CUSTOM_TEMPORARY_POWER_MODEL.UP.smartDescription" : "BASELIB-CUSTOM_TEMPORARY_POWER_MODEL.DOWN.smartDescription";
-        }
-    }
-
-    #endregion Localization
 
 }
